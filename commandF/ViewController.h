449fb3e8f253aca3d01f34fb6b9e1190eb1a25a7
peterliu1212 <peterliu1212@gmail.com>
2015-11-24 00:55:57 -0500

    Initial Commit

---
+++ commandF/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  commandF
+//
+//  Created by LiuChengYang on 11/24/15.
+//  Copyright © 2015 LiuChengYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ commandF/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  commandF
+//
+//  Created by LiuChengYang on 11/24/15.
+//  Copyright © 2015 LiuChengYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
